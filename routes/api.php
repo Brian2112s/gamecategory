49219b3efb7e1501db77f0cae1769c7867ec5bad
Brian2112s <125463013+Brian2112s@users.noreply.github.com>
2024-04-17 16:37:32 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GamecategoryController;
+use App\Http\Controllers\GameController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/gamecategories/{id}/games', GameController::class);
+Route::get('/games?sort={veld}', [GameController::class]);
 
+
+Route::apiResource('games', GameController::class);
+Route::apiResource('gamecategories', GamecategoryController::class);
