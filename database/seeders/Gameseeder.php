3466185c54f155ee8abef217d721247ee9587f81
JokerEren7 <127301336+JokerEren7@users.noreply.github.com>
2024-04-17 16:27:33 +0200

    s

--- database/seeders/Gameseeder.php
+++ database/seeders/Gameseeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\models\Game;
+use App\Models\Game;
 
 class Gameseeder extends Seeder
 {
